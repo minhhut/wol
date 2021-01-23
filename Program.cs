c63e8c08492f8f8370317e11199ed40619600e15
Minh Nguyen <minhhut@gmail.com>
2021-01-23 11:25:57 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,17 +10,13 @@ if (args.Length == 0)
 }
 else
 {
+    // original code from https://stackoverflow.com/a/65529803
+    
     var target = PhysicalAddress.Parse(args[0].Replace(".", "-").Replace(":", "-"));
-
     var header = Enumerable.Repeat((byte)0xff, 6);
-
     var data = Enumerable.Repeat(target.GetAddressBytes(), 16).SelectMany(mac => mac);
-
     var magicPacket = header.Concat(data).ToArray();
-
     using var client = new UdpClient();
-
     client.Send(magicPacket, magicPacket.Length, new IPEndPoint(IPAddress.Broadcast, 9));
-
     Console.WriteLine($"Sent out magic packet to [{args[0]}]");
 }
